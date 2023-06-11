ec6cd386ddca5c92d7a5b05924a0f159c7f98c14
Simone Farina <35815576+sfarina12@users.noreply.github.com>
2023-06-11 17:40:50 +0200

    1

--- js/navigation_system.js
+++ js/navigation_system.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){ 
     var act_selected = $(".nav_btn")[0]
+    var act_selected_tab = $(".tab")[0]
     $('.nav_btn_selected').css('--myVar', '21px');
 
     $(".nav_btn").click(function(){ 
@@ -15,6 +16,19 @@ $(document).ready(function(){
 
             act_selected = $(this)
             
+        }
+    })
+
+    $(".tab").click(function(){
+        if($(this) != act_selected_tab) {
+            
+            $(this).css('--myVar_2', '21px');
+            $(this).addClass("tab_selected")
+
+            $(act_selected_tab).removeClass("tab_selected")
+            $(act_selected_tab).css('--myVar_2', '0px');
+
+            act_selected_tab = $(this)
         }
     })
 })
